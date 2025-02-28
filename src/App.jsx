2cf30186a40603f9191d8a1ec28b50918c895f73
Prasad Jambhale <prasad.jambhale@nimapinfotech.com>
2025-02-28 22:16:52 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div>
-      <Toaster position="top-right" />
+      <Toaster position="top-bottom" />
       <RouterProvider router={router} />
     </div>
   );
